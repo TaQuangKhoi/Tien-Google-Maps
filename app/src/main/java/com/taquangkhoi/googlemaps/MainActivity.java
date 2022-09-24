4db206f44f9480ad40d33b5773cece30a2d68ed2
Tạ Quang Khôi <khoi0941@gmail.com>
2022-09-24 17:08:09 +0700

    Hiện dc màn hình Google Map

---
+++ app/src/main/java/com/taquangkhoi/googlemaps/MainActivity.java
@@ -0,0 +1,28 @@
+package com.taquangkhoi.googlemaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap mMap;
+    SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+}

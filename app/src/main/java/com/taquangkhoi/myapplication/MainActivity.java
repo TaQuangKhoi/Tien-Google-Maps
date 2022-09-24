4fef19c5fd131f72a9e7643f0a6b235fab25e511
Tạ Quang Khôi <khoi0941@gmail.com>
2022-09-24 23:32:00 +0700

    Chuyển Activity mới qua

--- app/src/main/java/com/taquangkhoi/myapplication/MainActivity.java
+++ app/src/main/java/com/taquangkhoi/myapplication/MainActivity.java
@@ -1,14 +1,110 @@
 package com.taquangkhoi.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap mMap;
+    SupportMapFragment mapFragment;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String type = spinner.getSelectedItem().toString();
+                switch (type) {
+                    case "Normal":
+                        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                        break;
+                    case "Satellite":
+                        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                        break;
+                    case "Terrain":
+                        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                        break;
+                    case "Hybrid":
+                        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    private void addControls() {
+        spinner = findViewById(R.id.Spinner);
+
+        // Tạo mảng để lưu dữ liệu
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Roadmap");
+        arrayList.add("Satellite");
+        arrayList.add("Hybrid");
+        arrayList.add("Terrain");
+
+        // ArrayAdapter dùng để hiển thị dữ liệu lên Spinner
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayList);
+        spinner.setAdapter(arrayAdapter);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Thêm nút Thu nhỏ phóng to
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Thêm cử chỉ thu nhỏ, phóng to - Zoom
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+
+        // Thêm My Location Button
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != getPackageManager().PERMISSION_GRANTED
+
+                &&
+
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != getPackageManager().PERMISSION_GRANTED)
+        {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
     }
 }

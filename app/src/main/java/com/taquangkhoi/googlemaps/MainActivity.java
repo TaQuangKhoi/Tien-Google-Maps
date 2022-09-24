ca540d65cc32d2f392811d1ca7bff08a41387ce4
Tạ Quang Khôi <khoi0941@gmail.com>
2022-09-24 18:16:29 +0700

    Fix lỗi
    Do Activity mà lại Extends Fragment

--- app/src/main/java/com/taquangkhoi/googlemaps/MainActivity.java
+++ app/src/main/java/com/taquangkhoi/googlemaps/MainActivity.java
@@ -4,25 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap mMap;
     SupportMapFragment mapFragment;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        addControls();
+        //addEvents();
+    }
+
+    private void addEvents() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String type = spinner.getSelectedItem().toString();
+                switch (type) {
+                    case "Normal":
+                        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                        break;
+                    case "Satellite":
+                        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                        break;
+                    case "Terrain":
+                        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                        break;
+                    case "Hybrid":
+                        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    private void addControls() {
+        //spinner = findViewById(R.id.Spinner);
+
+        // Tạo mảng để lưu dữ liệu
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("roadmap");
+        arrayList.add("satellite");
+        arrayList.add("hybrid");
+        arrayList.add("terrain");
+
+        // ArrayAdapter dùng để hiển thị dữ liệu lên Spinner
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayList);
+        //spinner.setAdapter(arrayAdapter);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }

f8465c933afea545e4deabe56f3172cad1d8fdd3
Tạ Quang Khôi <khoi0941@gmail.com>
2022-12-25 21:13:31 +0700

    Thành công dùng Firebase Auth

--- app/src/main/java/com/taquangkhoi/myapplication/LoginActivity.java
+++ app/src/main/java/com/taquangkhoi/myapplication/LoginActivity.java
@@ -4,20 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG = "LoginActivity";
+    TextView tvSingUp;
+    TextView tvSingUp2;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView tvSingUp = findViewById(R.id.tvSingUp);
-        TextView tvSingUp2 = findViewById(R.id.tvSingUp2);
+        tvSingUp = findViewById(R.id.tvSingUp);
+        tvSingUp2 = findViewById(R.id.tvSingUp2);
         tvSingUp.setOnClickListener(onClickSignup());
         tvSingUp2.setOnClickListener(onClickSignup());
+        btnLogin = findViewById(R.id.btn_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = tvSingUp.getText().toString();
+                String password = tvSingUp2.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    return;
+                } else {
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, task -> {
+                                if (task.isSuccessful()) {
+                                    Log.i(TAG, "onComplete: " + task.getResult().getUser().getUid());
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Log.i(TAG, "onComplete: " + task.getException().getMessage());
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Log.i(TAG, "onStart: " + currentUser.getUid());
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Log.i(TAG, "onStart: null");
+        }
     }
 
     private View.OnClickListener onClickSignup() {
@@ -25,7 +74,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-                LoginActivity.this.startActivity(intent);
+                startActivity(intent);
             }
         };
     }
--- app/src/main/java/com/taquangkhoi/myapplication/SignupActivity.java
+++ app/src/main/java/com/taquangkhoi/myapplication/SignupActivity.java
@@ -2,21 +2,72 @@ package com.taquangkhoi.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignupActivity extends AppCompatActivity {
+    TextView tvLogin;
+    TextView edtEmail;
+    TextView edtPassword;
+    TextView edtConfirmPassword;
+    Button btnSignup;
+    private static final String TAG = "SignupActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        TextView tvLogin = findViewById(R.id.tvLogin);
+        tvLogin = findViewById(R.id.tvLogin);
+
+
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        edtConfirmPassword = findViewById(R.id.edt_confirm_password);
+        btnSignup = findViewById(R.id.btn_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
         tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SignupActivity.this.finish();
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+                String confirmPassword = edtConfirmPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    return;
+                } else {
+                    if (password.equals(confirmPassword)) {
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(SignupActivity.this, task -> {
+                                    if (task.isSuccessful()) {
+                                        Log.i(TAG, "onComplete: " + task.getResult().getUser().getUid());
+                                        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Log.i(TAG, "onComplete: " + task.getException().getMessage());
+                                    }
+                                });
+                    }
+                    else {
+                        Log.i(TAG, "onClick: password not match");
+                        return;
+                    }
+                }
             }
         });
     }
